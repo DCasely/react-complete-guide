546c5b987f67310d43b707becf8be74df8bb580b
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-18 14:23:19 -0400

    Classes versus Hooks

--- src/App.js
+++ src/App.js
@@ -1,81 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
-// import names from './Person/names';
 
-class App extends Component {
-  state = {
+const App = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { id: 1, name: 'Jake', age: 28 },
       { id: 2, name: 'Jack', age: 40 },
       { id: 3, name: 'Bill', age: 31 },
     ],
-    otherState: 'some other value',
-  };
+  });
 
-  switchNameHandler = () => {
-    // console.log('Was Clicked');
-    // DON'T DO THIS:M this.state.persons[0].name = 'Davin';
+  const [otherState, setOtherState] = useState('some other value');
 
-    this.setState({
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { id: 1, name: 'Davin', age: 30 },
         { id: 2, name: 'John', age: 55 },
         { id: 3, name: 'Sarah', age: 37 },
       ],
     });
+
+    setOtherState('Changed Other State');
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        {/* 
-        {names.map((person) => (
-          <Person
-            id={person.id}
-            key={person.id}
-            name={person.name}
-            age={person.age}
-          />
-        ))} */}
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
 
-        {/* <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
+      {personsState.persons.map((person) => (
         <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
+          id={person.id}
+          key={person.id}
+          name={person.name}
+          age={person.age}
         />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        /> */}
+      ))}
 
-        {this.state.persons.map((person) => (
-          <Person
-            id={person.id}
-            key={person.id}
-            name={person.name}
-            age={person.age}
-          />
-        ))}
-
-        <Person name="Manu" age="29">
-          My Hobbies: Racing
-        </Person>
-      </div>
-    );
-
-    //   return React.createElement(
-    //     'div',
-    //     { className: 'App' },
-    //     React.createElement('h1', null, "Hi, I'm a React App!!!")
-    //   );
-  }
-}
+      <Person name="Manu" age="29">
+        My Hobbies: Racing
+      </Person>
+    </div>
+  );
+};
 
 export default App;
+
+// state = {
+//   persons: [
+//     { id: 1, name: 'Jake', age: 28 },
+//     { id: 2, name: 'Jack', age: 40 },
+//     { id: 3, name: 'Bill', age: 31 },
+//   ],
+//   otherState: 'some other value',
+// };
+
+// switchNameHandler = () => {
+//   // console.log('Was Clicked');
+//   // DON'T DO THIS:M this.state.persons[0].name = 'Davin';
+
+//   this.setState({
+//     persons: [
+//       { id: 1, name: 'Davin', age: 30 },
+//       { id: 2, name: 'John', age: 55 },
+//       { id: 3, name: 'Sarah', age: 37 },
+//     ],
+//   });
+// };
