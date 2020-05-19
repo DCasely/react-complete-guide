1ed13a95ab0fc90c0d9be2748f58def78246e889
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-19 11:03:42 -0400

    rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -5,31 +5,25 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { id: 1, name: 'Jake', age: 28 },
+      { id: 1, name: 'Davin', age: 30 },
       { id: 2, name: 'Jack', age: 40 },
       { id: 3, name: 'Bill', age: 31 },
     ],
-    otherState: 'some other value',
+    showAccounts: false,
   };
 
-  switchNameHandler = (updatedName) => {
-    // console.log('Was Clicked');
-    // DON'T DO THIS:M this.state.persons[0].name = 'Davin';
-    this.setState({
-      persons: [
-        { id: 1, name: updatedName, age: 30 },
-        { id: 2, name: 'John', age: 55 },
-        { id: 3, name: 'Sarah', age: 37 },
-      ],
-    });
+  showAccounts = () => {
+    const show = this.state.showAccounts;
+
+    this.setState({ showAccounts: !show });
   };
 
   changeNameHandler = (e) => {
     this.setState({
       persons: [
         { id: 1, name: e.target.value, age: 30 },
-        { id: 2, name: 'John', age: 55 },
-        { id: 3, name: 'Sarah', age: 37 },
+        { id: 2, name: e.target.value, age: 55 },
+        { id: 3, name: e.target.value, age: 37 },
       ],
     });
   };
@@ -52,6 +46,8 @@ class App extends Component {
       border: '3px solid black',
       padding: '8px',
       cursor: 'pointer',
+      display: 'block',
+      margin: '1rem auto',
     };
 
     const styleReset = {
@@ -67,35 +63,29 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button
-          style={styleSwitchName}
-          onClick={() => this.switchNameHandler('DC 5000')}
-        >
-          Switch Name
+        <button style={styleSwitchName} onClick={this.showAccounts}>
+          Show Names
         </button>
 
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          changeName={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-
-        <Person
-          click={this.switchNameHandler.bind(this, 'SUPERMAN')}
-          changeName={this.changeNameHandler}
-          name="Manu"
-          age="29"
-        >
-          My Hobbies: Racing
-        </Person>
+        {this.state.showAccounts && (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              changeName={this.changeNameHandler}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changeName={this.changeNameHandler}
+            />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+              changeName={this.changeNameHandler}
+            />
+          </div>
+        )}
 
         <button style={styleReset} onClick={this.resetHandler}>
           RESET
