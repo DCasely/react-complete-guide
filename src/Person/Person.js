81fd9ca4cae5ed5a189bfdce6876fbf17525d4b0
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-20 15:03:36 -0400

    removed styled components from Person folder

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
-// import './Person.css';
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import './Person.css';
 
 const person = (props) => {
   return (
-    // <div className="Person" style={style}>
-    <StyledDiv>
+    <div className="Person">
       <p>
         My name is {props.name} and I am {props.age} years old.
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changeName} value={props.name} />
       <button onClick={props.click}>Delete</button>
-    </StyledDiv>
+    </div>
   );
 };
 
