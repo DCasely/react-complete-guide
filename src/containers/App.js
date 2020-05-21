a03fb2db4878376053458f62bd0c04a920f959a0
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-21 15:00:04 -0400

    component update lifestyle (for state Changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,6 +32,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   showAccounts = () => {
     const show = this.state.showAccounts;
 
